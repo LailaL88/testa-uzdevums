4fb92850c1e0f0134c77aa5cc69ee9f48c6417e9
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-09-14 18:30:00 +0300

    Add files via upload

---
+++ assets/js/theme.js
@@ -0,0 +1,49 @@
+/* Open navbar when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close navbar when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+$(document).ready(function () {
+  // Add smooth scrolling
+  $("a").on("click", function (event) {
+    console.log(event);
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        4000,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
+//Show the icon on scroll
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > $("body").height() / 100) {
+    $("#upArrow").fadeTo(500, 1);
+  }
+});
+
+$("#upArrow").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+});
